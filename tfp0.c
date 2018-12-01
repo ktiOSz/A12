192d24efe57c7bc4d6069456f7a32c5e160d8380
Christian Colucci <37155945+ktiOSz@users.noreply.github.com>
2018-12-01 17:21:16 +0100

    Create tfp0.c

---
+++ tfp0.c
@@ -0,0 +1,9 @@
+{A12}
+We got tfp0!!! Read the kernel_base: 0xfffffff019404000 :
+0xfffffff019404000 : 100000cfeedfacf
+0xfffffff019404008 :       200000002
+0xfffffff019404010 :    14f80000001a
+0xfffffff019404018 :          200001
+0xfffffff019404020 :     27800000019
+0xfffffff019404028 :    545845545f5f
+0xfffffff019404030 :               0
